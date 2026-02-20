ebb838d4140bc07562c532eefd4e8d44e350ca4b
tarundangeti <tarun.dangeti2@gmail.com>
2026-02-20 14:47:22 +0530

    Add files via upload

---
+++ scripts/nginxprojrct_withpython/dc.sh
@@ -0,0 +1,6 @@
+mkdir -p /usr/local/lib/docker/cli-plugins/
+
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+-o /usr/local/lib/docker/cli-plugins/docker-compose
+
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
