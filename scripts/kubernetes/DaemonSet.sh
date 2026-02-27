3f546807dac402ba2324243e73bb0f3bf282e2e7
tarundangeti <tarun.dangeti2@gmail.com>
2026-02-27 22:08:48 +0530

    Add DaemonSet configuration for mobile banking app

---
+++ scripts/kubernetes/DaemonSet.sh
@@ -0,0 +1,18 @@
+apiVersion: apps/vq
+kind: DaemonSet
+metadata:
+  name: mb-daemonset
+  labels:
+    app: bank
+spec:
+  selector:
+    matchLabels:
+      app: bank
+  template:
+    metadata:
+      labels:
+        app: bank
+  spec:
+    containers:
+      - name: conti1
+        image: tarundangeti/mobilebankingimg
