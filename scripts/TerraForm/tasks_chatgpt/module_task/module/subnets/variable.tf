673aa6b199628e4221779d74afa4fedd9568f3b8
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:21:59 +0530

    Add subnet variables to variable.tf

---
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/subnets/variable.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "public_subnet_name" {}
+variable "tf_public_subnet_cidr" {}
+variable "public_az" {}
+variable "private_subnet_name" {}
+variable "tf_private_subnet_cidr" {}
+variable "private_az" {}
