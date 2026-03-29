32b61adb037d791330a17dfcfabe4e33a75ed75b
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-29 23:38:18 +0530

    Update AWS instance type to t2.small
    Changed instance type from t2.medium to t2.small in AWS instance configuration.

--- scripts/TerraForm/tasks_chatgpt/task1/main.tf
+++ scripts/TerraForm/tasks_chatgpt/task1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  instance_type = "t2.medium" # changing from t2.micro to t2.medium
+  instance_type = "t2.small" # changing from t2.micro to t2.small
   ami = "ami-0c3d1e0f8ec487d6d"
   key_name = "tarunpem5/02/2025"
   tags = {
