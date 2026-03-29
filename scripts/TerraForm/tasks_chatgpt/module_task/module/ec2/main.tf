faf2cf8eec82a69a612d90a5198cb525249a9a4f
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:37:39 +0530

    Add EC2 instance resource configuration

---
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "tf_instance" {
+  instance_type = var.instance_type
+  ami = var.instance_ami
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  key_name = var.key_name
+  tags = {
+    Name = var.sg_name
+  }
+}
