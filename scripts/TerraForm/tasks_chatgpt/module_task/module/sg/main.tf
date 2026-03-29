1318a6f094769b2198676249c024b8115676ec8a
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:27:55 +0530

    Add AWS security group with SSH and HTTP ingress rules

---
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/sg/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "tf_sg" {
+  vpc_id = var.vpc_id
+  ingress {
+    description = "this is to open SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+  ingress {
+    description = "this is to open HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [0.0.0.0/0]
+  }
+  tags = {
+    Name = var.sg_name
+  }
+}
