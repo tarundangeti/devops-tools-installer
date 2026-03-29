c24c4981d8bb3cfd97465bc449a340b79feecbbd
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:30:38 +0530

    Fix CIDR blocks formatting in main.tf
    Updated CIDR block syntax to use quotes.

--- scripts/TerraForm/tasks_chatgpt/module_task/module/sg/main.tf
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/sg/main.tf
@@ -5,20 +5,20 @@ resource "aws_security_group" "tf_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "this is to open HTTP"
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }  
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = var.sg_name
