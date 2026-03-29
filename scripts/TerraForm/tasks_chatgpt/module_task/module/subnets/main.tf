8702dabef4deaf0b971765c6a10cfca0748e7338
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:49:55 +0530

    Refactor subnets to include multiple public and private subnets

--- scripts/TerraForm/tasks_chatgpt/module_task/module/subnets/main.tf
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/subnets/main.tf
@@ -1,18 +1,36 @@
-resource "aws_subnet" "tf_public_subnet" {
+resource "aws_subnet" "tf_public_subnet1" {
   vpc_id = var.vpc_id
-  cidr_block = var.tf_public_subnet_cidr
-  availability_zone = var.public_az
+  cidr_block = var.tf_public_subnet_cidr1
+  availability_zone = var.public_az1
   map_public_ip_on_launch = true
   tags = {
-    Name = var.public_subnet_name
+    Name = var.public_subnet_name1
+  }
+}
+resource "aws_subnet" "tf_public_subnet2" {
+  vpc_id = var.vpc_id
+  cidr_block = var.tf_public_subnet_cidr2
+  availability_zone = var.public_az2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = var.public_subnet_name2
   }
 }
 
-resource "aws_subnet" "tf_private_subnet" {
+resource "aws_subnet" "tf_private_subnet1" {
   vpc_id = var.vpc_id
-  cidr_block = var.tf_private_cidr
-  availabillity_zone = var.private_az
+  cidr_block = var.tf_private_cidr1
+  availabillity_zone = var.private_az1
   tags = {
-    Name = var.private_subnet_name
+    Name = var.private_subnet_name1
+  }
+}
+
+resource "aws_subnet" "tf_private_subnet2" {
+  vpc_id = var.vpc_id
+  cidr_block = var.tf_private_cidr2
+  availabillity_zone = var.private_az2
+  tags = {
+    Name = var.private_subnet_name2
   }
 }
