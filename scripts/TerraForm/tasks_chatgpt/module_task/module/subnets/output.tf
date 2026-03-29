a8de49270f8cb6772a5047df0311199c67ae9c02
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-30 01:53:18 +0530

    Rename subnet output variables for clarity

--- scripts/TerraForm/tasks_chatgpt/module_task/module/subnets/output.tf
+++ scripts/TerraForm/tasks_chatgpt/module_task/module/subnets/output.tf
@@ -1,7 +1,7 @@
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "public_ids" {
+  value = [aws_subnet.tf_public_subnet1.id, aws_subnet.tf_public_subnet2.id]
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "private_ids" {
+  value = [aws_subnet.tf_private_subnet1.id, aws_subnet.tf_private_subnet2.id]
 }
